28662abc0ce163f5f0ff9bac7ad93fdbff1b0f54
Achasnyk <a.chasnyk@gmail.com>
2022-09-03 17:04:43 +0300

    deploy added

---
+++ public/slider.js
@@ -0,0 +1,29 @@
+let index = 1;
+showSlides(index);
+
+function handleSlide(n) {
+    showSlides(index += n);
+}
+
+function currentSlide(n) {
+    showSlides(index = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName('slides');
+    let markers = document.getElementsByClassName('markers_item');
+    if (n > slides.length) { index = 1 }
+    if (n < 1) { index = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    for (i = 0; i < markers.length; i++) {
+        markers[i].className = markers[i].className.replace(" active", "");
+    }
+    slides[index - 1].style.display = "block";
+   markers[index - 1].className += " active";
+    
+}
+
